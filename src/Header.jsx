402fc6b8775cfc7fdd7e84440c179eedcdf2d817
Lalk4 <57485454+Lalk4@users.noreply.github.com>
2021-02-12 23:12:56 +0500

    Revert "Merge branch 'css-modules' into dev"
    This reverts commit 5f49d9a0f3df55efc946477d9f4218754f5578d9, reversing
    changes made to 798e9b903d52cbe53eb074c655ae71dfe08c6cd8.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import {hot} from "react-hot-loader/root";
-import styles from "./header.scss"
+import * as React from 'react';
 
-function HeaderComponent() {
+export function Header() {
   return (
-    <header className={styles.example}>
-      <h1>Hello React</h1>
+    <header>
+      <h1>Reddit for our own</h1>
     </header>
   );
 }
-
-export const Header = hot(HeaderComponent);
